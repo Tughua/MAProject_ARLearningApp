772f85e150d3c687b5e935725c2c96952bd8c669
Tughu AIyewa <aiyewatughu@gmail.com>
2023-11-19 01:21:28 +0000

    Signpost Update
    Included sign post labels for more model parts.
    
    Completed show/hide labels with C# code.

---
+++ MAProject_AR/Assets/Scripts/AnnotDisplay.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleVisibility : MonoBehaviour
+{
+    public GameObject targetObject; // Assign your GameObject to this field
+    public float holdTime = 1f; // Adjust this value for the hold time required
+
+    private float touchStartTime;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchStartTime = Time.time; // Record the start time when touch begins
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                float touchDuration = Time.time - touchStartTime;
+
+                // Check if the touch duration is greater than the hold time
+                if (touchDuration >= holdTime)
+                {
+                    ToggleObjectVisibility();
+                }
+            }
+        }
+    }
+
+    void ToggleObjectVisibility()
+    {
+        if (targetObject != null)
+        {
+            // Toggle the visibility by setting the opposite of the current state
+            targetObject.SetActive(!targetObject.activeSelf);
+        }
+    }
+}
+
